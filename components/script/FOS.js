5781e627659f86679d82aab1f80aa5985ca5baa3
Fabio <nando.sanvido@gmail.com>
2013-02-07 21:22:53 +0100

    preliminary finel version

--- components/script/FOS.js
+++ components/script/FOS.js
@@ -1,14 +1,9 @@
 function openDB(data, callback){
     console.log('Going to open a IndexedDB database');
-//    console.log('-> ' + data);
-    
     var idbreq = indexedDB.open("com-agiledever-bablabl-20", 1);
-          
     idbreq.onerror = function(){console.log('ERROR opening the database');};//general_idb_error_handler;
-//    console.log('');
     idbreq.onupgradeneeded = function(event) {
         console.log('IDB ugrade needed!!');
-//        console.log(event.target);
         var db = idbreq.result;
         console.log('Trying to add an object store');
         var objectStore = db.createObjectStore("files", {autoIncrement: true});
@@ -23,17 +18,27 @@ function openDB(data, callback){
     };
 }
 
+function emptyDB(){
+    var transaction = db.transaction(["files"], "readwrite");
+    var req = transaction.objectStore("files").clear();
+    req.onerror = function(event){console.log('ERROR clearing the objectStore');};
+    req.onsuccess = function(event){
+        Lungo.Notification.success(
+            "OK",                  //Title
+            "Eliminados todos los datos",     //Description
+            "check",                    //Icon
+            3                          //Time on screen
+        );
+                    
+    };
+}
 
 function getNewImg() {
     var a = new MozActivity({ name: "pick", data: {type: ["image/jpeg", "image/png"]} });
-//    for (property in a) {
-//        console.log( property + '--> ' + a[property]+'; ');
-//    }
     a.onerror = function (event) { console.log('ERROR on picking a photo'); };
     a.onsuccess = function (event) {
         console.log('SUCCESS');  
         
-//        var last = $$('article#gallery img:nth-last-child(1)').attr('src').split('.')[0];
         console.log('Returned blob: ' + a.result.blob);
         var fReader = new FileReader();       
         fReader.readAsDataURL(a.result.blob);
@@ -46,9 +51,7 @@ function getNewImg() {
         fReader.onload = function (event){
             console.log('');
             $$('article#gallery').append('<img class="shadow" src="' + event.target.result + '">');
-//            console.log('RESULT --> ' + event.target.result);
             console.log('');
-//            storeNew(event.target.result);
             if (window.db === undefined){
                 openDB(event.target.result, storeNew);
             }
@@ -56,112 +59,68 @@ function getNewImg() {
                 storeNew(event.target.result);
             }  
         };
-        console.log('');
-        
+        console.log('');  
     };
 }
 
 function storeNew(img)
 {    
-//    console.log('TEST --> \n' + img);
     var idx = img.indexOf('base64');
     img=img.slice(idx+7, img.lenght);
-//    console.log('TEST --> ' + img);
-//    console.log('TEST --> \n' + img.replace(/.*base64,/g,''));
     
-//    console.log('START encryption');
-    var store = new Blob([sjcl.encrypt(window.key, img)]);
-//    console.log('END encryption');
+//    var store = new Blob([sjcl.encrypt(window.key, img)]);
+    var store = new Blob([sjcl.encrypt(localStorage.getItem('fundationKey'), img)]);
+    
     var transaction = db.transaction(["files"], "readwrite");
     transaction.objectStore("files").add(store);
-                         
-    
-/**************** Upgrade implemetation
-
-//    console.log('TESTING WORKERS');
-    var worker = new Worker('components/script/aes.js');
-    worker.postMessage({msg: 'data', data: img});
-    worker.onmessage=function(event){
-//        console.log('Received message from worker!!! --> ' + event.data);
-        if(event.data.cmd == 'ready'){
-//            console.log('Now we will store an encrpted version of image');
-//            console.log('received --> ' + event.data.cipher);
-           
-            var transaction = db.transaction(["files"], "readwrite");
-            transaction.objectStore("files").add(event.data.cipher);
-        }
-    };
-*******************/
 }
 
 function loadDBImages()
 {
-   var transaction = db.transaction(["files"]);
+    var transaction = db.transaction(["files"]);
     
+    Lungo.Element.loading('article#gallery',1);
     var imgsReq = transaction.objectStore("files").openCursor();
-    imgsReq.onerror = function(){console.log('Error getting IDB cursor');};
+    imgsReq.onerror = function(){
+        console.log('Error getting IDB cursor');
+        Lungo.Element.loading('article#gallery',0);
+        Lungo.Notification.error(
+                "ERROR",              
+                "Error accessing stored files..sorry",
+                "cancel",
+                3
+            );
+    };
     imgsReq.onsuccess = function(event)
         {
            var cursor = event.target.result;
             if (cursor) {
                 console.log("key for cursor " + cursor.key);
                 
+                
                 var fReader = new FileReader();       
                 fReader.readAsBinaryString(cursor.value);
                 fReader.onerror = function(event){console.log('ERROR loading a file');};
-                fReader.onloadstart = function(event){console.log('START TO load a file');};
-                fReader.onprogress = function(event){console.log('LOADING a file');};
+//                fReader.onloadstart = function(event){console.log('START TO load a file');};
+//                fReader.onprogress = function(event){console.log('LOADING a file');};
                 fReader.onloadend = function(event){console.log('LOAD END EVENT..');};
                 
                 fReader.onload = function (event){
-//                    console.log('LOAD event, result -->\n' + sjcl.decrypt(window.key, event.target.result));
+                    console.log('LOAD event')
                     var src='data:application/octet-stream;base64,';
-                    $$('article#gallery').append('<img id="image-' + cursor.key + '" class="shadow" src="' + src.concat(sjcl.decrypt(window.key, event.target.result)) + '">');
+//                    $$('article#gallery').append('<img id="image-' + cursor.key + '" class="shadow" src="' + src.concat(sjcl.decrypt(window.key, event.target.result)) + '">');
+                    $$('article#gallery').append('<img id="image-' + cursor.key + '" class="shadow" src="' + src.concat(sjcl.decrypt(localStorage.getItem('fundationKey'), event.target.result)) + '">');
                 }
-                
-//                $$('article#gallery').append('<img id="image-' + cursor.key + '" class="shadow" src="' + cursor.value + '">');
-//                    console.log('RESULT --> ' + event.target.result);
-                
                 cursor.continue();
-            }
-                else {
+                
+            } else {                
                 console.log("No more entries!");
+                Lungo.Element.loading('article#gallery',0);
             } 
         }
-    
-    
-    
-/********************************************  When we store Blobs without encryption
-    var transaction = db.transaction(["files"]);
-    
-    var imgsReq = transaction.objectStore("files").openCursor();
-    imgsReq.onerror = function(){console.log('Error getting IDB cursor');};
-    imgsReq.onsuccess = function(event)
-        {
-            var cursor = event.target.result;
-            if (cursor) {
-                console.log("key for cursor " + cursor.key);
-                var fReader = new FileReader();
-                fReader.readAsDataURL(cursor.value);
-                fReader.onloadend = function(event)
-                    {
-                    $$('article#gallery').append('<img id="image-' + cursor.key + '" class="shadow" src="' + event.target.result + '">');
-//                    console.log('RESULT --> ' + event.target.result);
-                };
-                
-                cursor.continue();
-            }
-                else {
-                console.log("No more entries!");
-            }
-        }
-**********************************************/
 }
 
 function loadImgs(){
     console.log('loading images...');
-    if(window.db === undefined)
-        openDB(null, loadDBImages);
+    if(window.db === undefined){openDB(null, loadDBImages);}else{loadDBImages();}
 }
-
-//sjcl.encrypt("password", "data");
--- components/script/app.js
+++ components/script/app.js
@@ -4,20 +4,109 @@ Lungo.init({
 });
 
 Lungo.ready(function() {
-    window.key = "1234567890"
-    loadImgs();
+//    window.key = "1234567890";
+    console.log('LUNGO READY');
+    if(localStorage.getItem('fundationKey')){
+        $$('a#newuser').hide();    
+    }
+    
 });
 
 Lungo.Events.init({
     'tap section#main header nav a:nth-child(2)':function(event){
         getNewImg();
+    },
+    'tap section#main header nav a:nth-child(3)':function(event){
+        $$('article#gallery img').remove();
+        for(i=0;i<$$('section#login form input').length;i++){
+            $$('section#login form input')[i].value='';
+        }
+//        while(Lungo.Router.History.current() != 'login'){
+            Lungo.Router.back();
+//        }
+    },
+    'tap a#in':function(event){
+        if(localStorage.getItem('fundationKey') === undefined || localStorage.getItem('fundationKey') == null){
+            Lungo.Notification.error(
+                "ERROR",              
+                "No existe ningun usuario",
+                "cancel",
+                3
+            );
+            return;
+        }
+        var user = $$('article#log form input:nth-child(1)').val();
+        var pas = $$('article#log form input:nth-child(2)').val();
         
+        var bitArray = sjcl.hash.sha256.hash(user.concat(pas));  
+        var digest_sha256 = sjcl.codec.hex.fromBits(bitArray);
+        
+        if(localStorage.getItem('fundationKey') !== digest_sha256){
+            Lungo.Notification.error(
+                "ERROR",              
+                "Usuario o contraseña incorecta",
+                "cancel",
+                3
+            );
+            return;
+        }
+
+        Lungo.Router.section('main');
+        loadImgs();
+    },
+    'tap article#sets #change':function(event){
+        Lungo.Notification.confirm({
+            icon: 'warning',
+            title: 'Cuidado',
+            description: 'Cambiando el usuario o la contraseña, todos los datos serán eliminados.',
+            accept: {
+                label: 'Eliminar datos',
+                callback: function(){
+                    Lungo.Notification.hide();
+                    $$('a#newuser').show();
+                    delete localStorage.fundationKey;
+                    emptyDB();
+                    $$('section#login article').toggleClass('current');
+                    while(Lungo.Router.History.current() != 'login'){
+                        Lungo.Router.back();
+                    }
+                }
+            },
+            cancel: {
+                label: 'No quiero!'
+            }
+        });
+    },
+    'tap a#new':function(event){
+        var user = $$('article#newpass form input:nth-child(1)').val();
+        var pas1 = $$('article#newpass form input:nth-child(2)').val();
+        var pas2 = $$('article#newpass form input:nth-child(3)').val();
+        
+        var error = "noerror";
+        if(!pas1 || !pas2){
+            error = "Campo contraseña vacío";
+        } else if(!(pas1 === pas2)){
+            error = "Las contraseñas no coinciden";
+        } else if(!user){
+            error = "Nombre de usuario necesario";
+        }
+        
+        if (error !== "noerror"){
+            Lungo.Notification.error(
+                "ERROR",              
+                error,
+                "cancel",
+                3
+            );
+            return;
+        }
+        
+        var bitArray = sjcl.hash.sha256.hash(user.concat(pas1));  
+        var digest_sha256 = sjcl.codec.hex.fromBits(bitArray);
+        
+        localStorage.setItem('fundationKey', digest_sha256);
+        
+        Lungo.Router.section('main');
+        loadImgs();
     }
 });
-
-//print all DeviceStorageCursor properties
-//obj=navigator.getDeviceStorage("pictures").enumerate();for (property in obj) {console.log( property + '--> ' + obj[property]+'; ');}
-//obj=navigator.getDeviceStorage("pictures").enumerate();for (property in obj) {console.log( property + '--> ' + obj[property]+'; ');}
-//navigator.getDeviceStorage("pictures").enumerate().addEventListener('readyState', function(event){console.log('READY --> '+event);});
-
-//JSON.stringify(obj);
